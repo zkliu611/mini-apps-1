c6b4284b62aa18090e30f4f511dc5ae2fa68f7de
ZK Liu <zkliu@ZKs-Mac.local>
2018-12-11 15:27:21 -0800

    set up index.html and server

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,15 @@
+$(document).ready(function(){
 
+  // function readFormData() { 
+  //   var formData = {};
+  //   formData['firstName'] = document.getElementById('first-name').value;
+  //   formData['lastName'] = document.getElementById('last-name').value;
+  //   formData['county'] = document.getElementById('county').value;
+  //   formData['city'] = document.getElementById('city').value;
+  //   formData['role'] = document.getElementById('role').value;
+  //   formData['sales'] = Number(document.getElementById('sales').value);
+  //   formData['children'] = document.getElementById('children').value;
+  //   return formData;
+  // }
 
+})
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,23 @@
+var path = require('path');
+var express = require('express');
+var app = express();
 
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, './client')));
+
+app.post('/cvs', (req, res) => {
+  console.log('serving post request');
+  res.redirect('/');
+  res.status(201);
+});
+
+app.get('/cvs', (req, res) => {
+  console.log('serving get request')
+  res.status(200).send('hello world');
+});
+
+var PORT = 8080;
+app.listen(PORT, () => console.log(`listening on port ${PORT}!`))
