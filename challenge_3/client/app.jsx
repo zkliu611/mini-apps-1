0ac81972e1078c9cd65d15b8cc63a5d0c2cc82c7
ZK Liu <zkliu@ZKs-Mac.local>
2018-12-12 12:28:38 -0800

    finish set up

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,18 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
 
+  render() {
+    return (
+      <div>
+       hello albert
+      </div>
+    );
+  }
+  
+}
 
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
 
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './client')));
 
+app.post('/', (req, res) => {
+
+})
+
+app.get('/', (req, res) => {
+  
+})
+
+var PORT = 8080;
+app.listen(PORT, () => console.log(`listening on port ${PORT}!`))
