dba251d3c42be19da170555844af888d4f99a3ca
ZK Liu <zkliu@ZKs-Mac.local>
2018-12-11 19:44:10 -0800

    server convert data to csv

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -7,17 +7,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, './client')));
 
+
+
+
 app.post('/csv', (req, res) => {
   console.log('serving post request');
-  console.log(req.body.formData);
+  var data = JSON.parse(req.body.formData);
+  var heads = Object.keys(data).slice(0, 6);
+  var rows = [];
+  var getRows = function(node) {
+    let row = Object.values(node).slice(0, 6);
+    rows.push(row);
+    if (node.children) {
+      for (let i = 0; i< node.children.length; i++){
+        getRows(node.children[i]);
+      }
+    }
+  }
+  getRows(data);
+  console.log(rows);
   res.redirect('/');
-  res.status(201);
-  res.end();
+  res.status(201).end();
 });
 
 app.get('/csv', (req, res) => {
   console.log('serving get request')
-  console.log(req.query.formData)
   res.redirect('/');
   res.status(200).send();
 });
