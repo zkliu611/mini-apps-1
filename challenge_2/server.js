f6fe57ef973d4e16cece9f2661663cf2bfd55a90
ZK Liu <zkliu@ZKs-Mac.local>
2018-12-11 16:48:36 -0800

    able to send data from client to sever

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,22 +1,25 @@
 var path = require('path');
 var express = require('express');
 var app = express();
-
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, './client')));
 
-app.post('/cvs', (req, res) => {
+app.post('/csv', (req, res) => {
   console.log('serving post request');
+  console.log(req.body.formData);
   res.redirect('/');
   res.status(201);
+  res.end();
 });
 
-app.get('/cvs', (req, res) => {
+app.get('/csv', (req, res) => {
   console.log('serving get request')
-  res.status(200).send('hello world');
+  console.log(req.query.formData)
+  res.redirect('/');
+  res.status(200).send();
 });
 
 var PORT = 8080;
